0deae6f6189b43cae9166fc45e6d53ec9c81cd17
tom <tgpetrie@gmail.com>
2019-11-22 09:57:19 -0800

    added loop

--- billy.js
+++ billy.js
@@ -1,19 +1,31 @@
 'use strict';
-var x = confirm('Press OK if you are not a robot.');
-var responseTime = prompt('What time is it? (in hours <= 24)');
-var greeting;
-if (responseTime > 18) {
-  greeting = 'Good evening';
-} else if (responseTime > 12) {
-  greeting = 'Good afternoon';
-} else if (responseTime > 0) {
-  greeting = 'Good morning';
-} else {
-  greeting = 'Welcome';
+
+function userGreeting() {
+  var x = confirm('Press OK if you are not a robot.');
+  var responseTime = prompt('What time is it? (in hours <= 24)');
+  var greeting;
+  if (responseTime > 18) {
+    greeting = 'Good evening';
+  } else if (responseTime > 12) {
+    greeting = 'Good afternoon';
+  } else if (responseTime > 0) {
+    greeting = 'Good morning';
+  } else {
+    greeting = 'Welcome';
+  }
+  if (x === true) {
+    var responseName = prompt('What is your name?');
+    document.write('<h3>' + greeting + ', ' + responseName + '!' + '</h3>');
+  } else {
+    document.write('<h3>' + greeting + '! ' + 'You are not allowed in here. Self-destruct initiated!' + '</h3>');
+  
+  } 
 }
-if (x === true) {
-  var responseName = prompt('What is your name?');
-  document.write('<h3>' + greeting + ', ' + responseName + '!' + '</h3>');
-} else {
-  document.write('<h3>' + greeting + '! ' + 'You are not allowed in here. Self-destruct initiated!' + '</h3>');
+function TShirtInfo() {
+  var TShirtNum = prompt('How many T-Shirts do you want?');
+  for (var i = 1; i <= TShirtNum; i++) {
+    var TShirtAmt = prompt('How many T-Shirts would you like in saved account' + i + '?');
+    document.write( i + '. #' + TShirtAmt + ' ');
+  }
 }
+
